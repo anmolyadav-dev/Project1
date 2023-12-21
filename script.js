1cd528f224cf65123914512900bb28dd9d27c0f0
Anmol Yadav <anmolyadav03unnao@gmail.com>
2023-12-21 19:43:24 +0530

    finished

--- script.js
+++ script.js
@@ -40,8 +40,6 @@ document.addEventListener('mouseleave',()=>{
   minicircle.style.opacity = "0";
 });
 
-
-
 // .......................................................................................
 let xprev = 0;
 let yprev = 0;
@@ -49,31 +47,33 @@ let timeout;
 let xscale =1 , yscale =1 ;
 
 window.addEventListener("mousemove",function(dets){
- 
+
   
   let circleMouseFollower = ()=>{
-    if(minicircle.style.transform===0 )  minicircle.style.transform = `scale(1)`;
+  if(minicircle.style.transform===0 )  minicircle.style.transform = `scale(1)`;
     minicircle.style.opacity = "1";
-    xloc = dets.x;
-    yloc = dets.y;
 
-    minicircle.style.left = `${xloc}px`;
-    minicircle.style.top = `${yloc}px`;
+    setTimeout(() => {
+      minicircle.style.left = `${dets.clientX}px`;
+    minicircle.style.top = `${dets.clientY}px`;
+    }, 100);
+
+    
 
 
     // circle compress during movement
     this.clearTimeout(timeout);
-    xscale = gsap.utils.clamp(0.8 , 1.2 , xloc - xprev);
-    yscale = gsap.utils.clamp(0.8 , 1.2 , yloc - yprev);
-    xprev = xloc;
-    yprev = yloc;
+    xscale = gsap.utils.clamp(0.8 , 1.2 , dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8 , 1.2 , dets.clientY - yprev);
+    xprev = dets.clientX;
+    yprev = dets.clientY;
     minicircle.style.transform = `translate(-50%,-50%) scale(${xscale} , ${yscale})`;
 
     // after stopping the cursor getting circle again
 
     timeout = setTimeout(() => {
       minicircle.style.transform = `translate(-50%,-50%) scale(1)`;
-    }, 100);
+    }, 200);
 
    
      
@@ -86,3 +86,43 @@ window.addEventListener("mousemove",function(dets){
 
 
 firstPageAnimation();
+
+// .............................. hover photo followere effect
+ 
+document.querySelectorAll(".elem").forEach(elem =>{
+  let rotate=0,diff=0;
+
+  elem.addEventListener("mouseleave",()=>{
+    gsap.to(elem.querySelector("img"),{
+      opacity:0,
+      ease : Power3,
+      duration:0.5,
+    })
+  })
+
+  elem.addEventListener("mousemove" , (dets)=> {
+
+    //elem animation
+
+
+    //photo tracking animation
+    diff= dets.clientX - rotate;
+    console.log(diff);
+    rotate = dets.clientX;
+
+    gsap.to(elem.querySelector("img"),{
+      opacity: 1,
+      ease: Power3,
+
+      top: dets.clientY - elem.getBoundingClientRect().top ,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20,20,diff*0.6),
+      duration:0.5
+      
+      
+    })
+
+  })
+  
+})
+
